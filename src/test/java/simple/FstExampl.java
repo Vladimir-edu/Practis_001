eccd2382a8fd5802ab539bea5bf922599d13c6d2
Vladimir-edu <Vladimir.wrk@yandex.ru>
2018-12-02 19:29:47 +0300

    examle_01

---
+++ src/test/java/simple/FstExampl.java
@@ -0,0 +1,19 @@
+package simple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//@RunnWith(Suite.class)
+public class FstExampl {
+    @Test
+    public void test1(){
+        System.out.println("Test1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Test2");
+    }
+
+
+}
